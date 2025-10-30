bfc7c9cb470f90311b9c238034db8ef2da3b7270
Muhammad Wahaj <wahajsiddiqui07@gmail.com>
2025-10-30 22:51:10 +0500

    minor changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,8 +6,8 @@ import { Amplify } from "aws-amplify";
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: process.env.VITE_USER_POOL_ID!,
-      userPoolClientId: process.env.VITE_USER_POOL_CLIENT_ID!,
+      userPoolId: import.meta.env.VITE_USER_POOL_ID!,
+      userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID!,
     },
   },
 });
